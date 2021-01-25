1c822ade720df1a72fd34c0040d673376b2dd788
Flavio Oliveira <flaoliveira@uolinc.com>
2021-01-25 17:39:44 -0300

    desafio 12.9

--- src/main/java/com/algaworks/algafood/api/controller/UserController.java
+++ src/main/java/com/algaworks/algafood/api/controller/UserController.java
@@ -8,7 +8,6 @@ import javax.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -70,13 +69,5 @@ public class UserController {
     public void update(@PathVariable Long userId, @RequestBody @Valid UserPasswordVO userPasswordVO) {
         userService.updatePassword(userId, userPasswordVO);
     }
-
-    @DeleteMapping("/{userId}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long userId) {
-        userService.delete(userId);
-    }
-
-    
 
 }
